62d2a7a2c6f2eba6d3cc14697b8a0bddc4df548a
RewBugLag <rewrew2000@gmail.com>
2019-09-07 16:54:21 +0700

    Added Vending with Array Lab

---
+++ VendingMER/TestVendingMachine.java
@@ -0,0 +1,94 @@
+/**
+ * TestVendingMachine
+ */
+public class TestVendingMachine {
+
+    public static void main(String[] args) {
+        VendingMachine vendA = new VendingMachine(10, "grape");
+        // admin session
+        System.out.println("[ admin session test ]\n");
+
+        System.out.println("set maximum to: " + vendA.setMaximumStorage(5));
+        System.out.println(vendA.getMaximum());
+        /*
+        vendA.setAll(0, "orange", 10, 1000);
+        System.out.println("set product: " + vendA.getProductTypeAd());
+        System.out.println("set price to: " + vendA.getProductPriceAd());
+        System.out.println("set total to: " + vendA.getProductTotalAd());
+        System.out.println();
+        */
+        vendA.setProductType(0, "orange");
+        System.out.println("set product: " + vendA.getProductTypeAd());
+        vendA.setProductType(4, "grape");
+        System.out.println("set product: " + vendA.getProductTypeAd());
+        System.out.println();
+
+        vendA.setProductPrice(10, "orange");
+        System.out.println("set price to: " + vendA.getProductPriceAd());
+        vendA.setProductTotal(1000, "orange");
+        System.out.println("set total to: " + vendA.getProductTotalAd());
+        System.out.println();
+
+        vendA.setProductPrice(20, "grape");
+        System.out.println("set price to: " + vendA.getProductPriceAd());
+        vendA.setProductTotal(4000, "grape");
+        System.out.println("set total to: " + vendA.getProductTotalAd());
+        /*
+        System.out.println(vendA.productType[4]);
+        System.out.println(vendA.productPrice[4]);
+        System.out.println(vendA.productTotal[4]);
+        */
+        /*
+        vendA.setAll(4, "grape", 40, 4000);
+        System.out.println("set product: " + vendA.getProductTypeAd());
+        System.out.println("set price to: " + vendA.getProductPriceAd());
+        System.out.println("set total to: " + vendA.getProductTotalAd());
+        System.out.println();
+        */
+        /*
+        System.out.println(vendA.productType[4]);
+        vendA.setProductType(4, "grape"); // index = 4 = grape
+        System.out.println(vendA.getProductTypeAd());// index = orange = -1 not found // need get productType index for admin
+        vendA.setProductType(0, "orange");
+        System.out.println(vendA.getProductTypeAd());// index = orange
+
+        System.out.println(vendA.productPrice[4]);
+        vendA.setProductPrice(30, "grape");
+        System.out.println(vendA.getProductPriceAd());// index = orange
+        
+        System.out.println(vendA.productTotal[4]);
+        vendA.setProductTotal(1000, "grape");
+        System.out.println(vendA.productTotal[0]);
+        System.out.println(vendA.getProductTotalAd());// index = orange still... it's get (product)
+
+        System.out.println(); // (product) still "orange" index still "orange" in get
+
+        vendA.setProductType(0, "orange"); // index change from grape to orange
+        vendA.setProductType(4, "grape"); // just change the index not product | index and product are pointer
+        
+        System.out.println(vendA.getProductType());
+        vendA.setProductPrice(10, "orange");
+        System.out.println("set price to: " + vendA.getProductPrice());
+        vendA.setProductTotal(1000, "orange");
+        System.out.println("set product to: " + vendA.getProductTotal());
+        vendA.setMoney(100);
+        System.out.println("set money to: " + vendA.getVendingMoney() + "\n");
+        */
+        // user session
+        System.out.println("[ user session test ]\n");
+        System.out.println("current money inserted: " + vendA.getMoneyInserted());
+        vendA.receiveMoney(10);
+        System.out.println("second inserted: " + vendA.getMoneyInserted());
+        //vendA.setProductPrice(10, "grape");
+        System.out.println("product price: " + vendA.getProductPrice());
+        System.out.println("request for product with เงินทอน: " + vendA.giveProduct());
+        System.out.println("\n----------Machine side-----------");
+        System.out.println("current product: " + vendA.getProductType());
+        System.out.println("current product amount: " + vendA.getProductTotal());
+        System.out.println("current money in machine: " + vendA.getVendingMoney());
+        System.out.println("---------------------------------\n");
+        System.out.println("cancel request: " + vendA.cancel());        
+    }
+}
+
+// what if central set method?
---
+++ VendingMER/VendingMachine.java
@@ -0,0 +1,239 @@
+/*
+-1 = no product | invalid index
+-2 = !enough
+-3 = 404 product not found
+*/
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class VendingMachine {
+    // instance's input
+    private int moneyInserted;
+    private String product;
+    // local instance
+    private int index;
+    private int indexAd;
+    private int maximum;
+    private int vendingMoney;
+    private boolean inserted;
+    // productDB
+    /* this was just for ez test at first | now you can set it by yourself
+    int[] productPrice = {10, 20, 30, 40};
+    String[] productType = {"orange", "grape", "apple", "banana"};
+    int[] productTotal = {1, 2, 3, 4};
+    */ 
+    int[] productPrice;
+    String[] productType;
+    int[] productTotal;
+
+    public VendingMachine(int moneyInserted, String product) {
+        this.moneyInserted = moneyInserted;
+        this.product = product;
+        
+    }
+
+    public int setMaximumStorage(int maximum) {
+        this.maximum = maximum;
+        productPrice = new int[maximum];
+        productType = new String[maximum];
+        productTotal = new int[maximum];
+        return maximum;
+
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+    ///////////////////////////////////////////////////////////////////
+    public int getSpecProductPrice(String yProduct) {
+        findingProduct(yProduct);
+        if (index == -1) {
+            return -3;
+        }
+        return productPrice[index];
+    }
+
+    public int getSpecProductTotal(String yProduct) {
+        findingProduct(yProduct);
+        if (index == -1) {
+            return -3;
+        }
+        return productTotal[index];
+    }
+    ///////////////////////////////////////////////////////////////////
+    public int getProductPriceAd() {
+        if (indexAd == -1) {
+            return -3;
+        }
+        return productPrice[indexAd];
+    }
+
+    public String getProductTypeAd() {
+        if (indexAd == -1) {
+            return null;
+        }
+        return productType[indexAd];
+    }
+
+    public int getProductTotalAd() {
+        if (indexAd == -1) {
+            return -3;
+        }
+        return productTotal[indexAd];
+    }
+    ///////////////////////////////////////////////////////////////////
+    public int getProductPrice() {
+        findingProduct(product);
+        if (index == -1) {
+            return -3;
+        }
+        return productPrice[index];
+    }
+
+    public String getProductType() {
+        findingProduct(product);
+        if (index == -1) {
+            return null;
+        }
+        return productType[index];
+    }
+
+    public int getProductTotal() {
+        findingProduct(product);
+        if (index == -1) {
+            return -3;
+        }
+        return productTotal[index];
+    }
+    ///////////////////////////////////////////////////////////////////
+    public int getVendingMoney() {
+        return vendingMoney;
+    }
+
+    public int getMoneyInserted() {
+        return moneyInserted;
+    }
+    ///////////////////////////////////////////////////////////////////
+
+    public boolean receiveMoney(int amount) {
+        if (amount < 0) {
+            return false;
+
+        } else {
+            moneyInserted += amount;
+            inserted = true;
+            return true;
+        }
+    }
+
+    // need implementation for Arrays data type
+    public int giveProduct() {
+        findingProduct(product);
+        if (index == -1) {
+            return -3; // product not found
+
+        } else if (productTotal[index] == 0) {
+            return -1; // no product
+
+        } else if (moneyInserted < productPrice[index]) {
+            return -2; // !enough money
+
+        } else if (moneyInserted == productPrice[index]) {
+            productTotal[index]--;
+            vendingMoney += productPrice[index];
+            return 0; // no เงินทอน
+
+        } else {
+            productTotal[index]--; // product decreased by 1 as the order can be only 1 each
+            vendingMoney += productPrice[index]; // money in machine increased
+            return returnMoney();
+        }
+    }
+
+    public int returnMoney() {
+        return moneyInserted - productPrice[index]; // calculate เงินทอน
+    }
+
+    public int cancel() {
+        return moneyInserted;
+    }
+    ///////////////////////////////////////////////////////////////////
+
+    // need implementation for Array[index]
+    public void setAll(int i, String yProduct, int price, int total) { // for first time setting
+        setProductType(i, yProduct);
+        setProductPrice(price, yProduct);
+        setProductTotal(total, yProduct);
+    }
+
+    public boolean setProductType(int i, String yProduct) {
+        if (yProduct == null || i < 0 || i >= maximum) { // productType.length
+            indexAd = -1; // invalid index input
+            return false;
+
+        } else {
+            productType[i] = yProduct;
+            indexAd = i; //
+            return true;
+        }
+    }
+
+    public boolean setProductPrice(int amount, String yProduct) {
+        if (amount < 0) {
+            return false;
+        } else {
+            findingProduct(yProduct);
+            indexAd = index;
+            productPrice[index] = amount;
+            return true;
+        }
+    }
+
+    public boolean setProductTotal(int amount, String yProduct) {
+        if (amount < 0) {
+            return false;
+        } else {
+            findingProduct(yProduct);
+            indexAd = index;
+            productTotal[index] = amount;
+            return true;
+        }
+    }
+
+    public boolean setMoney(int amount) {
+        if (amount < 0) {
+            return false;
+        } else {
+            vendingMoney = amount;
+            return true;
+        }
+    }
+    ///////////////////////////////////////////////////////////////////
+
+    // This is product finder [indexOf]
+    public boolean findingProduct(String xProduct) {
+        for (int i = 0; i < productType.length; i++) {
+            if (xProduct == productType[i]) {
+                index = i;
+                return true;
+            }
+        }
+        index = -1;
+        return false;
+    }
+
+    public String toString() {
+        StringBuilder vendingString = new StringBuilder();
+        vendingString.append("{ Current money : ");
+        if (inserted) {
+            vendingString.append(this.moneyInserted);
+            vendingString.append(" bath, Current order : ");
+            vendingString.append(this.product);
+        } else {
+            vendingString.append("0 bath, Current order : none");
+        }
+        
+        vendingString.append(" }\n");
+        return vendingString.toString();
+    }
+}
